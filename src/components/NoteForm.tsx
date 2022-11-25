407883250ecb9b983f82585b9e9feb9a8a6e8c5d
Howard Ding <howardding2000@gmail.com>
2022-11-25 15:57:31 +0000

    rename the app, and style NoteForm

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -29,7 +29,7 @@ const NoteForm: React.FC<NoteFormProps> = ({ onSubmit }) => {
           <Col>
             <Form.Group controlId='title'>
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} required className='shadow-none' />
             </Form.Group>
           </Col>
           <Col>
@@ -47,13 +47,25 @@ const NoteForm: React.FC<NoteFormProps> = ({ onSubmit }) => {
                   );
                 }}
                 isMulti
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    boxShadow: "none",
+                  }),
+                }}
               />
             </Form.Group>
           </Col>
         </Row>
         <Form.Group controlId='markdown'>
           <Form.Label>Body</Form.Label>
-          <Form.Control ref={markdownRef} required as='textarea' rows={15} />
+          <Form.Control
+            ref={markdownRef}
+            required
+            as='textarea'
+            rows={15}
+            className='shadow-none'
+          />
         </Form.Group>
         <Stack direction='horizontal' gap={2} className='justify-content-end'>
           <Button type='submit' variant='primary'>
